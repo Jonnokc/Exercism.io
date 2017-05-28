1eff849aa2147568f161eeceef4d2fd77be47154
Jonnokc <jonnoadams@gmail.com>
2017-05-28 16:23:40 -0500

    updated

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -3,6 +3,6 @@ def is_pangram(sentence=''):
     import string
 
     for char in string.ascii_lowercase:
-        if str.lower(char) not in str.lower(sentence):
+        if char not in str.lower(sentence):
             return False
     return True
