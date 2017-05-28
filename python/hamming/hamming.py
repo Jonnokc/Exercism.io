727d99e7b0a9d8b74ccd91ebaf6ffe7f47317458
Jonnokc <jonnoadams@gmail.com>
2017-05-28 18:51:59 -0500

    completed hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,13 @@
-def distance():
-    pass
+def distance(dna_1, dna_2):
+
+    dna_1 = list(dna_1)
+    dna_2 = list(dna_2)
+    dif_count = 0
+
+    if len(dna_1) != len(dna_2):
+        raise ValueError()
+
+    for index, elem in enumerate(dna_1):
+        if dna_1[index] != dna_2[index]:
+            dif_count += 1
+    return dif_count
