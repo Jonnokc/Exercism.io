dabfa3b4b25864e5a2a8fa22a6cb8e79f0e0089d
Jonnokc <jonnoadams@gmail.com>
2017-05-28 14:44:25 -0500

    completed Isogram problem

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,12 @@
-def is_isogram():
-    pass
+def is_isogram(word=''):
+
+    for char in word:
+        count = 0
+        for let in word:
+            if (str.isalpha(char) is True and
+                    str.isalpha(let) is True and
+                    str.upper(char) == str.upper(let)):
+                count = count + 1
+        if count >= 2:
+            return False
+    return True
