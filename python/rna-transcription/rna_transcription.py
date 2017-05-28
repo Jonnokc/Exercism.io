1d66bb99d2cea85f63c90e4b0001e8e2c1164eee
Jonnokc <jonnoadams@gmail.com>
2017-05-28 17:09:04 -0500

    Uploaded solution

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,17 @@
-def to_rna():
-    pass
+def to_rna(dna=''):
+
+    new_dna = list(dna)
+
+    for i in range(len(new_dna)):
+        if new_dna[i] == 'G':
+            new_dna[i] = 'C'
+        elif new_dna[i] == 'C':
+            new_dna[i] = 'G'
+        elif new_dna[i] == 'T':
+            new_dna[i] = 'A'
+        elif new_dna[i] == 'A':
+            new_dna[i] = 'U'
+        else:
+            return ('')
+
+    return ''.join(new_dna)
