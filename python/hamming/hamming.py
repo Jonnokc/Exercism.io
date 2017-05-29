ebf4ff6b893ffc8dff7d7aa88e9538c2d595bfd6
Jonnokc <jonnoadams@gmail.com>
2017-05-28 19:05:35 -0500

    re-wrote to optimize

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,13 +1,10 @@
 def distance(dna_1, dna_2):
 
-    dna_1 = list(dna_1)
-    dna_2 = list(dna_2)
     dif_count = 0
-
-    if len(dna_1) != len(dna_2):
+    if len(dna_1) == len(dna_2):
+        for dna_1, dna_2 in zip(dna_1, dna_2):
+            if dna_1 != dna_2:
+                dif_count += 1
+        return dif_count
+    else:
         raise ValueError()
-
-    for index, elem in enumerate(dna_1):
-        if dna_1[index] != dna_2[index]:
-            dif_count += 1
-    return dif_count
