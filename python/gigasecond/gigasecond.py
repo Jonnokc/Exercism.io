a19dfff7a7ece3324a28878d30730d5904ab1aff
Jonnokc <jonnoadams@gmail.com>
2017-07-01 16:10:33 -0500

    completed gigasecond

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,2 +1,15 @@
-def add_gigasecond():
-    pass
+# Calculate the moment when someone has lived for 109 seconds.
+#
+# A gigasecond is 10^9 (1,000,000,000) seconds.
+
+
+from datetime import timedelta
+
+
+def add_gigasecond(user_date):
+
+    difference = timedelta(seconds=10**9)
+
+    new_date = user_date + difference
+
+    return new_date
