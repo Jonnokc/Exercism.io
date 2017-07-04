81ff17af5adb30ff5547bfc8950724cb5fc8953d
jonnokc <jonnoadams@gmail.com>
2017-07-04 18:14:17 -0500

    pushed Bob solution

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,14 @@
-def hey():
-    pass
+def hey(sentence):
+
+    sentence = sentence.strip()
+    if sentence.isupper():
+        # Yelling
+        return 'Whoa, chill out!'
+    elif sentence.endswith('?'):
+        # Question
+        return 'Sure.'
+    elif sentence == '':
+        # Nothing
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
